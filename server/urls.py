ef5ce9058316c81b2413b44f1af315c54ecf345d
Igor Mosyagin <i.mosyagin@lamoda.ru>
2018-05-26 02:34:58 +0300

    Add basic django-graphql app with docker compose

---
+++ server/urls.py
@@ -0,0 +1,11 @@
+from django.conf import settings
+from django.urls import path, re_path, include, reverse_lazy
+from django.conf.urls.static import static
+from django.contrib import admin
+from django.views.generic.base import RedirectView
+from graphene_django.views import GraphQLView
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('graphql', GraphQLView.as_view(graphiql=True)),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
